95b3af32c3d249e9c5d02cad20d2105182c709eb
Pranavster <122951073+Pranavster@users.noreply.github.com>
2024-08-31 17:28:50 -0500

    Update storage_mgr.c
    storage_mgr.c can now create, open, close and destroy 1 file per fileHandle

--- storage_mgr.c
+++ storage_mgr.c
@@ -18,8 +18,9 @@ RC createPageFile(char *fileName)
     char *emptyPage = (char *)calloc(PAGE_SIZE, sizeof(char));//create and fill the empty page with the contents of the page file
     fwrite(emptyPage, sizeof(char), PAGE_SIZE, file);
     free(emptyPage);//free memory of emptyPage
-    fHandle->totalNumPages+=1;//notify the total number of pages has increased
+    //fHandle->totalNumPages+=1;//notify the total number of pages has increased
     fclose(file);
+    free(ptr);
     return RC_OK;//this means the function worked
 }
 RC openPageFile(char *fileName, SM_FileHandle *fHandle)
@@ -39,10 +40,12 @@ RC closePageFile(SM_FileHandle *fHandle)
 {
     if(fHandle->mgmtInfo == NULL)//if the file is non existent or closing it did not work, return RC_FILE_HANDLE_NOT_INIT or RC_FILE_NOT_CLOSED, respectively
     {
+        printf("Null management Info");
         return RC_FILE_HANDLE_NOT_INIT;
     }
     else if(fclose((FILE *)(fHandle->mgmtInfo))!=0)
     {
+        printf("Null management Info");
         return RC_FILE_NOT_CLOSED;
     }
     fHandle->mgmtInfo = NULL;//set the closePageFile to 0
@@ -51,39 +54,42 @@ RC closePageFile(SM_FileHandle *fHandle)
 }
 RC destroyPageFile(char *fileName)
 {
-    SM_FileHandle *fHandle;//declare fHandle and remove 1 page if possible
-    if(fHandle->totalNumPages>0)
+    SM_FileHandle fHandle;
+    RC result = openPageFile(fileName, &fHandle);
+    if(result != RC_OK)
     {
-        fHandle->totalNumPages-=1;
+        printf("\na\n");
+        return result;
     }
-    else//edge case for when this method is called if there are no pages
+    result = closePageFile(&fHandle);
+    if(result != RC_OK)
     {
-        printf("Can't remove a page that does not exist");
-        exit(1);
+        printf("\nb\n");
+        return result;
     }
-    if (remove(fileName) != 0) //if the file is not successfully removed, return appropriate macro, otherwise, free the file and return RC_OK
+    if(remove(fileName)!=0)
     {
+        printf("\nc\n");
         return RC_FILE_NOT_FOUND;
     }
-    free(fileName);
-    return RC_OK;
+    
 }
 
 int main() 
 {
     SM_FileHandle fHandle;
-    RC crpf_result = createPageFile("example.txt");/*Testing creation of file*/
-    if(crpf_result==RC_OK)
+
+    RC crpf1_result = createPageFile("koushik.txt");//Testing creation of file
+    if(crpf1_result==RC_OK)
     {
         printf("File successfully created");
     }
     else
     {
-        printf("File unsuccessfully created, result is ",crpf_result);
+        printf("File unsuccessfully created, result is %d\n",crpf1_result);
     }
-    
-    RC opf_result = openPageFile("example.txt", &fHandle);/*Testing opening of file*/
-    if(opf_result==RC_OK)
+    RC opf1_result = openPageFile("koushik.txt", &fHandle);//Testing opening of file
+    if(opf1_result==RC_OK)
     {
         printf("File successfully opened");
         printf("Filename %s\n",fHandle.fileName);
@@ -92,24 +98,27 @@ int main()
     }
     else
     {
-        printf("File unsuccessfully created, result is ",opf_result);
+        printf("File unsuccessfully opened, result is %d\n",opf1_result);
     }
-    RC cpf_result = closePageFile(&fHandle);/*Testing closing of file*/
+
+    RC cpf_result = closePageFile(&fHandle);//Testing closing of file
     if(cpf_result==RC_OK)
     {
-        printf("File successfully closed");
+        printf("File successfully closed\n");
     }
     else
     {
-        printf("File unsuccessfully created, result is ",cpf_result);
+        printf("File unsuccessfully closed, result is %d\n",cpf_result);
     }
-    RC dpf_result = destroyPageFile("example.txt");/*Testing destruction of file*/
+    RC dpf_result = destroyPageFile("koushik.txt");//Testing destruction of file
     if(dpf_result==RC_OK)
     {
-        printf("File successfully closed");
+        printf("File successfully destroyed\n");
     }
     else
     {
-        printf("File unsuccessfully created, result is ",dpf_result);
+        printf("File unsuccessfully destroyed, result is %d\n",dpf_result);
     }
+
+
 }
